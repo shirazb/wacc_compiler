4620045e21c5b26a735ea79c1d2f65af8df2adf9
Zubair Chowdhury <Zubair.Chowhury@bsigroup.com>
2016-09-01 12:44:49 +0100

    Unit tests for charLit,IntLit,pairLit

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,54 @@
 import Test.HUnit
 import System.Exit
+import Utility.Definitions
+import Utility.BasicCombinators
+import Parser
 main :: IO ()
 main = do
-  results <- runTestTT test1
+  results <- runTestTT allTests
   if errors results + failures results == 0
     then
-      exitSuccess
+      putStrLn "All passed"
     else
-      exitFailure
+      putStrLn "Something failed"
+
+allTests = TestList [testIntLiteralGroup, testBoolLiteralGroup, testCharLiteralGroup, testPairLiteralGroup]
+
+-- Test Groups
+testIntLiteralGroup = TestLabel "IntLiteralTests" $ TestList [test_intLit1, test_intLit2, test_intLit3, test_intLit4]
+testBoolLiteralGroup = TestLabel "BoolLiteralTests" $ TestList [test_boolLit1, test_boolLit2, test_boolLit3]
+testCharLiteralGroup = TestLabel "CharLiteralTests" $ TestList [test_charLit1, test_charLit2, test_charLit3, test_charLit4, test_charLit5, test_charLit6, test_charLit7, test_charLit8, test_charLit9, test_charLit10,
+                                                                test_charLit11, test_charLit12]
+testPairLiteralGroup = TestLabel "PairLiteralTests" $ TestList [test_pairLit1, test_pairLit2, test_pairLit3, test_pairLit4, test_pairLit5]                                                               
+-- Tests for intLiteral
+test_intLit1 = TestCase $ assertEqual "Parse (single digit)" [(IntLit 5, "")] (parse intLiteral "5")
+test_intLit2 = TestCase $ assertEqual "Parse (multi digit)" [(IntLit 512312, "")] (parse intLiteral "512312")
+test_intLit3 = TestCase $ assertEqual "Parse only digits" [(IntLit 512312, "dontparsethis")] (parse intLiteral "512312dontparsethis")
+test_intLit4 = TestCase $ assertEqual "Parse check if fails" [] (parse intLiteral "_12") 
 
 
+-- Tests for booLiteral.
+test_boolLit1 = TestCase $ assertEqual "Parse True" [(BoolLit True, "")] (parse boolLiteral "true")
+test_boolLit2 = TestCase $ assertEqual "Parse False" [(BoolLit False, "")] (parse boolLiteral "false")
+test_boolLit3 = TestCase $ assertEqual "Parse check if fails" [] (parse boolLiteral "trueshouldfail") --- interesting test case??
 
+-- Tests for charLiteral
+test_charLit1  = TestCase $ assertEqual "Parse one char" [(CharLit 'p',"arser")] (parse charLiteral "\'p\'arser")
+test_charLit2  = TestCase $ assertEqual "Parse escape char \\b" [(CharLit '\b', "rest")] (parse charLiteral "\'\\b\'rest")	
+test_charLit3  = TestCase $ assertEqual "Parse escape char \\n" [(CharLit '\n', "rest")] (parse charLiteral "\'\\n\'rest")
+test_charLit4  = TestCase $ assertEqual "Parse escape char \\f" [(CharLit '\f', "rest")] (parse charLiteral "\'\\f\'rest")
+test_charLit5  = TestCase $ assertEqual "Parse escape char \\r" [(CharLit '\r', "rest")] (parse charLiteral "\'\\r\'rest")
+test_charLit6  = TestCase $ assertEqual "Parse escape char \\t" [(CharLit '\t', "rest")] (parse charLiteral "\'\\t\'rest")
+test_charLit7  = TestCase $ assertEqual "Parse escape char \\" [(CharLit '\\', "rest")] (parse charLiteral "\'\\\\'rest")
+test_charLit8  = TestCase $ assertEqual "Parse escape char \"" [(CharLit '\"', "rest")] (parse charLiteral "\'\\\"\'rest")
+test_charLit9  = TestCase $ assertEqual "Parse escape char \'" [(CharLit '\'', "rest")] (parse charLiteral "\'\\'\'rest")
+test_charLit10 = TestCase $ assertEqual "Parse escape char \\" [(CharLit '0', "rest")] (parse charLiteral "\'\\0\'rest") -- interesting test case? do we want \NUl or \0
+test_charLit11 = TestCase $ assertEqual "Parse check if fails" [] (parse charLiteral "shouldfail")
+test_charLit12 = TestCase $ assertEqual "Parse check if fails" [] (parse charLiteral "\'thissouldfail")
 
-
-
-test1 = TestCase $ assertEqual "test" 2 2
-test2 = TestCase $ assertEqual "test" 2 2
-
-testGroup1 = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
+-- Tests for pairLiteral
+test_pairLit1 = TestCase $ assertEqual "Parse a pairLiteral" [(PairLiteral, "rest")] (parse pairLiteral "nullrest") -- this one or the one after?
+test_pairLit2 = TestCase $ assertEqual "Parse a pairLiteral" [] (parse pairLiteral "nullrest") -- what behavior do we want?
+test_pairLit3 = TestCase $ assertEqual "Parse a pairLiteral" [(PairLiteral, " rest")] (parse pairLiteral "null rest")
+test_pairLit4 = TestCase $ assertEqual "Parse check if fails pairLit" [] (parse pairLiteral "gnullrest")
+test_pairLit5 = TestCase $ assertEqual "Parse check if fails pairLit" [] (parse pairLiteral "nu llrest")
---
+++ test/Utility/Definitions.hs
@@ -0,0 +1,61 @@
+module Utility.Definitions where
+
+--- DATA DEFINITION FOR WACC HASKELL COMPILER PROJECT --
+
+-- WACC SYNTAX -- ABSTRACT SYNTAX TREE
+data Program = Program [Func] Stat deriving (Show, Eq)
+type Ident = String
+data Func    = Func Type Ident ParamList Stat deriving (Show, Eq)
+data ParamList = ParamList [Param] deriving (Show, Eq)
+data Param   = Param Type Ident deriving (Show, Eq)
+data Stat
+  = Skip
+  | Declaration Type Ident AssignRHS
+  | Assignment AssignLHS AssignRHS
+  | Read AssignLHS
+  | Free Expr
+  | Return Expr
+  | Exit Expr
+  | Print Expr
+  | Println Expr
+  | If Expr Stat Stat
+  | While Expr Stat
+  | Begin Stat
+  | Seq Stat Stat
+  deriving (Show, Eq)
+data AssignLHS = Var Ident | ArrayDeref ArrayElem | PairDeref PairElem deriving (Show, Eq)
+data AssignRHS
+  = Expression Expr
+  | NewArray ArrayLit
+  | NewPair Expr Expr
+  | PairElement PairElem
+  | FuncCall Ident ArgList
+  deriving (Show, Eq)
+data ArgList = Args [Expr] deriving (Show, Eq)
+data PairElem = First Expr | Second Expr deriving (Show, Eq)
+data Type = BaseT BaseType | ArrayT ArrayType | PairT PairType deriving (Show, Eq)
+data BaseType = BaseInt | BaseBool | BaseChar | BaseString deriving (Show, Eq)
+data ArrayType = ArrayType Type deriving (Show, Eq)
+data PairType = PairType PairElemType PairElemType deriving (Show, Eq)
+data PairElemType = BaseP BaseType | BaseA ArrayType | Pair deriving (Show, Eq)
+data Expr = StringLit String
+            | CharLit Char
+            | IntLit Int
+            | BoolLit Bool
+            | PairLiteral
+            | ExprI Ident
+            | ExprArray ArrayElem
+            | UnaryApp UnOp Expr
+            | BinaryApp BinOp Expr Expr
+            deriving (Show, Eq)
+
+data ArrayLit = ArrayLit [Expr] deriving (Show, Eq)
+data ArrayElem = ArrayElem Ident [Expr] deriving (Show, Eq)
+data UnOp    = Not | Neg | Len | Ord | Chr  deriving (Show, Eq)
+data BinOp   = Mul | Div | Mod | Add | Sub | AND | OR | LT | LTE | EQ | GTE | GT | NEQ  deriving (Show, Eq)
+
+keywords = ["while","if","else"]
+binOps   =  [("*", Mul),  ("/", Div), ("%", Mod), ("+", Add),
+             ("-", Sub),  (">", Utility.Definitions.GT),  (">=", GTE), ("<", Utility.Definitions.LT),
+             ("<=", LTE), ("==", Utility.Definitions.EQ), ("!=", NEQ), ("&&", AND),
+             ("||", OR)]
