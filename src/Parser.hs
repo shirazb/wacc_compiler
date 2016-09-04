5649281c10d66181cb86038c9b05a5dc45ef7987
Shiraz Butt <sb4515@ic.ac.uk>
2016-09-04 21:48:25 +0100

    removed unneccesary imports

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,16 +2,12 @@ module Parser (parseProgram) where
 
 import Control.Applicative
 import Control.Monad
-import Data.Char
-import Data.Maybe
 import Debug.Trace
 
 {- Local Imports -}
 
-import Parsers.Expression
 import Parsers.Function
 import Parsers.Statement
-import Parsers.Type
 import Utility.Definitions
 import Utility.BasicCombinators
 import Utility.Declarations
@@ -20,4 +16,4 @@ parseProgram :: Parser Program
 parseProgram
   = bracket (string "begin") parseProgram' (string "end")
   where
-    parseProgram' = liftM2 Program (many parseFunc) parseStatement
+    parseProgram' = liftM2 Program (many parseFunction) parseStatement
