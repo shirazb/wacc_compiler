b5c195623f8b80ec770eb62d0a904e7b0c6d31d8
Zubair Chowdhury <zubairchowdhury12@hotmail.co.uk>
2016-09-13 16:41:53 +0100

    Removed function debug statements

--- src/Parsers/Function.hs
+++ src/Parsers/Function.hs
@@ -13,28 +13,14 @@ import Utility.Declarations
 import Utility.Definitions
 import Debug.Trace
 
-
--- maybeParse p v = Parser $ \s -> case parse p s of
---                               res -> return res
---                               [] -> [([],s)]
-
--- we need to try define some sort maybe parser
-
 parseFunction :: Parser Func
 parseFunction = do
-  traceM "  WE ARE PARSING THE FUNCTION  "
   returnType <- parseType
-  traceM ("The returnType is: " ++ show returnType)
   name <- identifier
-  traceM ("The name of the function: " ++ show name)
   paramList <- bracket (punctuation '(') parseParamList (punctuation ')')
-  traceM ("The parameterList is: " ++ show paramList)
   keyword "is"
-  traceM "We are parsing the keyword is "
   funcBody <- parseStatement
-  traceM ("We are parsing the funcBody: " ++ show funcBody)
   keyword "end"
-  traceM "We are parsing the keyword end "
   return $ Func returnType name paramList funcBody
 
 -- are we accounting for the fact
