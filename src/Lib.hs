99976ea564e5aabb713d468b15dde43f65cd4d33
Sarah Baka <sarahbaka@dyn1196-246.wlan.ic.ac.uk>
2016-09-20 13:09:54 +0100

    Corrected comments and indentation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,7 @@
-{-This should be removed as soon as the YAML file is configured correctly after we finish the front end-}
+{- 
+This should be removed as soon as the YAML file is configured correctly after 
+we finish the front end 
+-}
 
 module Lib
     ( someFunc
