7ed49dac9cc068535e00df1981cb469486dab993
mzc14 <mzc14@ic.ac.uk>
2016-09-21 22:18:58 +0100

    Added template for endParsing function

--- src/Parser.hs
+++ src/Parser.hs
@@ -39,3 +39,34 @@ parseProgram
   = bracket (keyword "begin") parseProgram' (string "end")
   where
     parseProgram' = liftM2 Program (many parseFunction) parseStatement
+
+
+
+-- keyword :: String -> Parser String
+-- keyword k = do
+--   kword <- leadingWS (string k)
+--   check isSpace <|> check isPunctuation <|> check isComment
+--   junk
+--   return kword
+--
+-- endingParse = do
+--    string "end"
+--    junk s
+--    st <- get
+--    if length (st) != 0 then mzero else result "!"
+
+
+
+-- Y: end
+-- Y: end___
+-- N: endasuidhas
+-- N: end    sjdhasd
+-- Y: end   #asjkdhaskjda
+-- Y: end#dhasiudhasd
+
+
+-- specification for the function that we need at the end of parse Program
+-- is one that consumes the rest of the string and it fails if it finds
+-- something that is not whitespace or is not a comment
+
+-- sdgjgs \nsdjqws
