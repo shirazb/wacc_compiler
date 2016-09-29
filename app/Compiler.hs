d962ef35fd24ce4a49da38e43ec4432fd2eca2f1
mzc14 <mzc14@ic.ac.uk>
2016-09-29 14:35:38 +0100

    Exposed the necessary modules in .cabal file

--- app/Compiler.hs
+++ app/Compiler.hs
@@ -1,9 +1,8 @@
 import System.Environment
 import Parser
-import Utility.Declarations
 main = do
   args           <- getArgs
   let fileName   =  head args  
   program        <- readFile fileName
-  let ast        =  fst . head $ parse parseProgram program
+  let ast        =  runParser program
   print ast
