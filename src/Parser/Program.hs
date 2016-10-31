e63a3ab0da7d6a448f8d1c16802dd2edc70daa36
mzc14 <mzc14@ic.ac.uk>
2016-10-31 22:32:36 +0000

    [sb][zc] moved program parser from Main to Parser.Program

---
+++ src/Parser/Program.hs
@@ -0,0 +1,35 @@
+module Parser.Program (parseProgram) where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.State  (MonadState (..), StateT (..))
+import Control.Monad.Except
+
+import Parser.Expression
+import Parser.Function
+import Parser.Lexer
+import Parser.Statement
+import Parser.Combinators
+import Utilities.Declarations
+import Utilities.Definitions
+
+parseProgram :: Parser Char Program
+parseProgram
+  = bracket (tryParser (keyword "begin") "Invalid Program start")
+      parseProgram' endingParse
+  where
+    parseProgram'
+      = liftM2 Program (many parseFunction)
+          (tryParser parseStatement "Invalid or missing program body")
+
+endingParse :: Parser Char String
+endingParse
+  = do
+      tryParser (string "end") "Unexpected Symbol"
+      junk
+      unusedInputString <- get
+      pos               <- getPosition
+      if null unusedInputString
+        then return "Valid Program"
+        else throwError
+               ("Syntax Error: Invalid Program", updateRowPosition pos)
