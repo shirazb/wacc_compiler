e4e425edfe5c07affbd0cf87863bc66840609197
mzc14 <mzc14@ic.ac.uk>
2016-11-08 13:05:54 +0000

    [zc] implemented type checking to ensure arguments passed to a return statement in a function match the return type of the function.

--- src/Semantics/TypeChecker/Function.hs
+++ src/Semantics/TypeChecker/Function.hs
@@ -4,7 +4,28 @@ module Semantics.TypeChecker.Function (
 
 import Semantics.TypeChecker.Statement
 import Utilities.Definitions
+import Semantics.TypeChecker.Expression
+import Control.Monad.Writer.Strict
 
 typeCheckFunc :: Func -> TypeChecker ()
-typeCheckFunc (Func t name params body pos)
-  = typeCheckStat body
+typeCheckFunc (Func (FuncT rT plT) name params body pos)
+  = typeCheckStat body >> checkFunctionReturn body rT
+
+checkFunctionReturn :: Stat -> Type -> TypeChecker ()
+checkFunctionReturn (Skip _) expT
+  = return ()
+checkFunctionReturn (Return expr _) expT = do
+  t <- typeCheckExpr expr
+  when (t /= expT) (tell ["FUNCTION RETURN TYPE DOES NOT MATCH"])
+  return ()
+checkFunctionReturn (If _ s1 s2 _) expT = do
+  checkFunctionReturn s1 expT
+  checkFunctionReturn s2 expT
+checkFunctionReturn (While _ s1 _) expT
+  = checkFunctionReturn s1 expT
+checkFunctionReturn (Block s1 _) expT
+  = checkFunctionReturn s1 expT
+checkFunctionReturn (Seq s1 s2 _) expT = do
+  checkFunctionReturn s1 expT
+  checkFunctionReturn s2 expT
+checkFunctionReturn _ _ = return ()
