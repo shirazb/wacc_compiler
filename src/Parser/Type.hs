1aa3e7dd94f4410c447e6d0f28e0afc0ab472a44
sb4515 <sb4515@ic.ac.uk>
2016-11-10 18:40:38 +0000

    [sb] cleaned up Parser.Type

--- src/Parser/Type.hs
+++ src/Parser/Type.hs
@@ -6,46 +6,53 @@ types.
 
 module Parser.Type (parseType) where
 
-import Control.Monad
-import Control.Applicative
-import Data.Maybe
+import Control.Applicative (Alternative (..))
+import Data.Maybe          (fromJust)
 
+import Parser.Combinators
 import Parser.Identifier
 import Parser.Lexer
-import Parser.Combinators
 import Utilities.Definitions
 
 --POST: Parser of types for the WACC language, built up using the more basic
---      parsers of types. Returns type wrapped in appropriate data constructor.
+--      parsers of types.
 parseType :: Parser Char Type
 parseType
   =   parseArrayType
   <|> parsePairType
   <|> parseBaseType
 
+-- Parses the base types of WACC.
 parseBaseType :: Parser Char Type
 parseBaseType = do
-  baseTypeString <- foldr1 (<|>) (map (keyword . fst) baseTypes)
+  baseTypeString <- foldr1 (<|>) baseTypeKeywords
   return $ BaseT (fromJust (lookup baseTypeString baseTypes))
+  where
+    baseTypeKeywords = map (keyword . fst) baseTypes
 
-multiDimArray :: Parser Char Int
-multiDimArray
+-- Parses the '[]'s in an array type to get the dimension of the array.
+arrayDim :: Parser Char Int
+arrayDim
   = token "[]" >> rest 1
   where
     rest x = (do
       token "[]"
       rest (x + 1)) <|> return x
 
+-- Parses an array type. The innermost type of an array cannot be an array type.
 parseArrayType :: Parser Char Type
 parseArrayType = do
-  t         <-  parseBaseType <|> parsePairType
-  dimension <- multiDimArray
+  t         <- parseBaseType <|> parsePairType
+  dimension <- arrayDim
   return $ ArrayT dimension t
 
+-- Parses a nested pair type. These have no type information other than that
+-- they are a pair.
 parseNestedPairType :: Parser Char Type
 parseNestedPairType
     = token "pair" >> return Pair
 
+-- Parses a pair type.
 parsePairType :: Parser Char Type
 parsePairType = trimWS $ do
   token "pair"
@@ -56,6 +63,8 @@ parsePairType = trimWS $ do
   require (punctuation ')') "Missing closing parenthesis in pair-type"
   return $ PairT t1 t2
 
+-- Parses types that are allowed to be within a pair. This does not include
+-- types of the form pair(t, t').
 parseInnerPairType :: Parser Char Type
 parseInnerPairType
   = parseNestedPairType <|> parseArrayType <|> parseBaseType
