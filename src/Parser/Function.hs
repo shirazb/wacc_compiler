08a080f19258f2adb72d26f1559b19f4f42bbd0a
Tom Bellingham <tombellingtom@gmail.com>
2016-11-10 18:44:33 +0000

    [tb] Parser/Function.hs: order imports alphabetically

--- src/Parser/Function.hs
+++ src/Parser/Function.hs
@@ -6,12 +6,12 @@ module Parser.Function  where
 import Control.Monad ( liftM3 )
 import Control.Monad.Except ( throwError )
 
+import Parser.Combinators ( Parser, getPosition, require, sepby )
+import Utilities.Definitions
 import Parser.Identifier ( identifier )
 import Parser.Lexer
 import Parser.Statement ( parseStatement )
 import Parser.Type ( parseType )
-import Parser.Combinators ( Parser, getPosition, require, sepby )
-import Utilities.Definitions
 
 -- POST: Parses a function defintion.
 parseFunction :: Parser Char Func
