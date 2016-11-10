640075e78936a70c08b1f4f903b5f7e7475ecab3
Tom Bellingham <tombellingtom@gmail.com>
2016-11-10 17:39:51 +0000

    [tb] qualify Function.hs imports, identifier names, comments

--- src/Parser/Function.hs
+++ src/Parser/Function.hs
@@ -1,48 +1,43 @@
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE NoMonomorphismRestriction  #-}
-{-# LANGUAGE UndecidableInstances       #-}
-{-# LANGUAGE TypeFamilies               #-}
-
+{- 
+Parser combinators for functions.
+-}
 module Parser.Function  where
 
-import Control.Monad
-import Control.Monad.Except
-
+import Control.Monad ( liftM3 )
+import Control.Monad.Except ( throwError )
 import Parser.Lexer
-import Parser.Statement
-import Parser.Type
-import Parser.Combinators
+import Parser.Statement ( parseStatement )
+import Parser.Type ( parseType )
+import Parser.Combinators ( Parser, getPosition, require, sepby )
 import Utilities.Definitions
 
 parseFunctionBody :: Parser Char Stat
 parseFunctionBody = do
-  funcBody  <- parseStatement
-  parseStatAndCheckExecPathEnds funcBody
-  return funcBody
+  body  <- parseStatement
+  checkExecutionPath body
+  return body
 
 {-
   Should check patterns (Seq (Return _) _) and (Seq (Exit _) _), throwing an
   error; unreachable statements are not permitted.
 -}
-parseStatAndCheckExecPathEnds :: Stat -> Parser Char Stat
-parseStatAndCheckExecPathEnds s@Return{}
+checkExecutionPath :: Stat -> Parser Char Stat
+checkExecutionPath s@Return{}
  = return s
-parseStatAndCheckExecPathEnds s@Exit{}
+checkExecutionPath s@Exit{}
  = return s
-parseStatAndCheckExecPathEnds (If _ s1 s2 _)
- = parseStatAndCheckExecPathEnds s1 >> parseStatAndCheckExecPathEnds s2
-parseStatAndCheckExecPathEnds (While _ s1 _)
- = parseStatAndCheckExecPathEnds s1
-parseStatAndCheckExecPathEnds (Block s1 _)
- = parseStatAndCheckExecPathEnds s1
-parseStatAndCheckExecPathEnds (Seq Return{} _ _) = do
+checkExecutionPath (If _ s1 s2 _)
+ = checkExecutionPath s1 >> checkExecutionPath s2
+checkExecutionPath (While _ s1 _)
+ = checkExecutionPath s1
+checkExecutionPath (Block s1 _)
+ = checkExecutionPath s1
+checkExecutionPath (Seq Return{} _ _) = do
    pos <- getPosition
    throwError ("Unreachable statement after return", pos)
-parseStatAndCheckExecPathEnds (Seq s1 s2 _)
- = parseStatAndCheckExecPathEnds s2
-parseStatAndCheckExecPathEnds _ = do
+checkExecutionPath (Seq s1 s2 _)
+ = checkExecutionPath s2
+checkExecutionPath _ = do
   pos <- getPosition
   throwError ("Mising return or exit statement in function body ending at: ",
     pos)
@@ -59,16 +54,16 @@ parseFunction = do
   funcBody     <- require parseFunctionBody "Invalid function body"
   require (keyword "end") "Missing 'end' keyword"
   pos          <- getPosition
-  let paramTypes = map getTypeOfParam (getListOfParams paramList)
-  let functionType = FuncT returnType paramTypes
+  let parameterTypes = map parameterType (parameters paramList)
+  let functionType = FuncT returnType parameterTypes
   return $ Func functionType name paramList funcBody pos
 
-getTypeOfParam :: Param -> Type
-getTypeOfParam (Param t _ _)
+parameterType :: Param -> Type
+parameterType (Param t _ _)
   = t
 
-getListOfParams :: ParamList -> [Param]
-getListOfParams (ParamList list _) = list
+parameters :: ParamList -> [Param]
+parameters (ParamList list _) = list
 
 -- POST: Attempts to parse a list of parameters if there is one.
 parseParamList :: Parser Char ParamList
