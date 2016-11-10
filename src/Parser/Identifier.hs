d348419103845682c330e4209d0809215275d3f1
sb4515 <sb4515@ic.ac.uk>
2016-11-10 18:43:52 +0000

    [sb] commented functions in Parser.Identifier

--- src/Parser/Identifier.hs
+++ src/Parser/Identifier.hs
@@ -1,15 +1,19 @@
 module Parser.Identifier (identifier) where
 
-import Control.Monad        (liftM2, guard)
 import Control.Applicative  (Alternative (..))
-import Utilities.Definitions
+import Control.Monad        (liftM2, guard)
+
 import Parser.Combinators
 import Parser.Lexer
+import Utilities.Definitions
 
+-- Parses strings that are permitted to be identifiers.
 ident :: Parser Char String
 ident
   = liftM2 (:) (char '_' <|> letter) (many (alphanum <|> char '_'))
 
+-- Parses identifiers, removing surround whitespace and checking it is
+-- not a keyword.
 identifier :: Parser Char Ident
 identifier = trimWS $ do
   name  <- ident
