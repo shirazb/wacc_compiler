5979f67be6e3fef6eb7523ceed9344246eaec62f
Shiraz Butt <shiraz.b@icloud.com>
2016-11-20 03:15:33 +0000

    [sb] implemented codegen for functions

--- src/CodeGen/Function.hs
+++ src/CodeGen/Function.hs
@@ -13,6 +13,26 @@ import CodeGen.Expression
 import CodeGen.Statement
 import Utilities.Definitions
 
+-- TODO: Avoid code duplication with genInNewScope. Only difference
+--       is that this does addParamsToEnv after save.
 instance CodeGen Func where
-  codegen _
-    = return []
+  codegen (Func t ident (ParamList params _) body _) = do
+    save
+    addParamsToEnv params 0
+    let sizeOfScope = scopeSize body
+    (env, _) <- get
+    let envWithOffset = Map.map (+ sizeOfScope) env
+    put (envWithOffset, sizeOfScope)
+    let createStackSpace = [SUB NF SP SP (ImmOp2 sizeOfScope)]
+    instrs <- codegen body
+    let clearStackSpace = [ADD NF SP SP (ImmOp2 sizeOfScope)]
+    restore
+    return $ createStackSpace ++ instrs ++ clearStackSpace
+
+addParamsToEnv :: [Param] -> Int -> InstructionMonad ()
+addParamsToEnv (Param t (Ident name _) _ : ps) offsetToParam = do
+  (env, offset)         <- get
+  let newEnv            = Map.insert name offsetToParam env
+  let offsetToNextParam = offsetToParam + typeSize t
+  put (newEnv, offset)
+  addParamsToEnv ps offsetToNextParam
