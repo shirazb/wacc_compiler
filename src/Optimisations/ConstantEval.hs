bb6f99bed91e3b6bde23d05ae32c67bd55b80e21
Zubair Chowdhury <zubairchowdhury12@hotmail.co.uk>
2016-11-29 15:26:48 +0000

    [zc] Added monadic interface to constant evaluation optimiser to handle arithmetic errors

--- src/Optimisations/ConstantEval.hs
+++ src/Optimisations/ConstantEval.hs
@@ -1,50 +1,92 @@
 module Optimisations.ConstantEval where
 import Utilities.Definitions
-
-optConstantEval :: AST -> AST
-optConstantEval (Program fs body)
-  = Program fs (optConstEvalStat body)
-
-optConstEvalStat :: Stat -> Stat
-optConstEvalStat s@Skip{}
-  = s
-optConstEvalStat (Declaration t ident rhs pos)
-  = Declaration t ident (optConstEvalAssignRHS rhs) pos
-optConstEvalStat (Assignment lhs rhs pos)
-  = Assignment lhs (optConstEvalAssignRHS rhs) pos
-optConstEvalStat s@Read{}
-  = s
-optConstEvalStat (Free e pos)
-  = Free (optConstEvalExpr e) pos
-optConstEvalStat (Exit e pos)
-  = Exit (optConstEvalExpr e) pos
-optConstEvalStat (Print e pos)
-  = Print (optConstEvalExpr e) pos
-optConstEvalStat (Println e pos)
-  = Println (optConstEvalExpr e) pos
-optConstEvalStat (If cond s s' pos)
-  = If (optConstEvalExpr cond) (optConstEvalStat s) (optConstEvalStat s') pos
-optConstEvalStat (While cond body pos)
-  = While (optConstEvalExpr cond) (optConstEvalStat body) pos
-optConstEvalStat (Block s pos)
-  = Block (optConstEvalStat s) pos
-optConstEvalStat (Seq s s' pos)
-  = Seq (optConstEvalStat s) (optConstEvalStat s') pos
+import Control.Monad.Writer
 
 
-optConstEvalAssignRHS :: AssignRHS -> AssignRHS
-optConstEvalAssignRHS (ExprAssign e pos)
-  = ExprAssign (optConstEvalExpr e) pos
-optConstEvalAssignRHS (ArrayLitAssign es pos)
-  = ArrayLitAssign (map optConstEvalExpr es) pos
-optConstEvalAssignRHS (NewPairAssign e e' pos)
-  = NewPairAssign (optConstEvalExpr e) (optConstEvalExpr e') pos
-optConstEvalAssignRHS (PairElemAssign (PairElem s e pos) pos')
-  = PairElemAssign (PairElem s (optConstEvalExpr e) pos) pos'
-optConstEvalAssignRHS (FuncCallAssign i es pos)
-  = FuncCallAssign i (map optConstEvalExpr es) pos
+{-
 
-optConstEvalExpr :: Expr -> Expr
+TODO: Add arithmetic ErrorMessages
+TODO: Add control flow analysis
+-}
+
+type ArithmeticErrors = [String]
+optConstEvalAST :: AST -> Writer ArithmeticErrors AST
+optConstEvalAST (Program fs body) = do
+  body' <- optConstEvalStat body
+  return $ Program fs body'
+
+
+optConstEvalStat :: Stat -> Writer ArithmeticErrors Stat
+optConstEvalStat (Declaration t ident rhs pos) = do
+  rhs' <- optConstEvalAssignRHS rhs
+  return $ Declaration t ident rhs' pos
+
+optConstEvalStat (Assignment lhs rhs pos) = do
+  rhs' <- optConstEvalAssignRHS rhs
+  return $ Assignment lhs rhs' pos
+
+optConstEvalStat (Free e pos) = do
+  e' <- optConstEvalExpr e
+  return $ Free e' pos
+
+optConstEvalStat (Exit e pos) = do
+  e' <- optConstEvalExpr e
+  return $ Exit e' pos
+
+optConstEvalStat (Print e pos) = do
+  e' <- optConstEvalExpr e
+  return $ Print e' pos
+
+optConstEvalStat (Println e pos) = do
+  e' <- optConstEvalExpr e
+  return $ Println e' pos
+
+optConstEvalStat (If cond s1 s2 pos) = do
+  cond' <- optConstEvalExpr cond
+  s1' <- optConstEvalStat s1
+  s2' <- optConstEvalStat s2
+  return $ If cond' s1' s2' pos
+
+optConstEvalStat (While cond body pos) = do
+  cond' <- optConstEvalExpr cond
+  body' <- optConstEvalStat body
+  return $ While cond' body' pos
+
+optConstEvalStat (Block s pos) = do
+  s' <- optConstEvalStat s
+  return $ Block s' pos
+
+optConstEvalStat (Seq s1 s2 pos) = do
+  s1' <- optConstEvalStat s1
+  s2' <- optConstEvalStat s2
+  return $ Seq s1' s2' pos
+
+optConstEvalStat s
+  = return s
+
+optConstEvalAssignRHS :: AssignRHS -> Writer ArithmeticErrors AssignRHS
+optConstEvalAssignRHS (ExprAssign e pos) = do
+  e' <- optConstEvalExpr e
+  return $ ExprAssign e' pos
+
+optConstEvalAssignRHS (ArrayLitAssign es pos) = do
+  es' <- mapM optConstEvalExpr es
+  return $ ArrayLitAssign es' pos
+
+optConstEvalAssignRHS (NewPairAssign e1 e2 pos) = do
+  e1' <- optConstEvalExpr e1
+  e2' <- optConstEvalExpr e2
+  return $ NewPairAssign e1' e2' pos
+
+optConstEvalAssignRHS (PairElemAssign (PairElem s e pos) pos') = do
+  e' <- optConstEvalExpr e
+  return $ PairElemAssign (PairElem s e' pos) pos'
+
+optConstEvalAssignRHS (FuncCallAssign i params pos) = do
+  params' <- mapM optConstEvalExpr params
+  return $ FuncCallAssign i params' pos
+
+optConstEvalExpr :: Expr -> Writer ArithmeticErrors Expr
 optConstEvalExpr (BinaryApp (Arith Add) (IntLit i pos') (IntLit i' pos) posE)
   = IntLit (i + i') pos'
 
@@ -108,4 +150,4 @@ optConstEvalExpr (ExprArray (ArrayElem ident es pos) pos')
   = ExprArray (ArrayElem ident (map optConstEvalExpr es) pos) pos'
 
 optConstEvalExpr e
-  = e
+  = return e
